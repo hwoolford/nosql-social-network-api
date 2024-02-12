6073eebf1c9c6523ebe2f7dc3a3ff87eb456a13a
forxtoisate <hwoolford@gmail.com>
2024-02-12 14:40:39 -0600

    Updated thought model

---
+++ models/Thought.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: new ObjectId,
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max: 280,
+        },
+        username: { type: String, required: true},
+        createdAt: { type: Date, default: Date.now },
+    }
+);
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min: 1,
+            max: 280,
+        },
+        createdAt: { type: Date, default: Date.now },
+        username: { type: String, required: true },
+        reactions: [reactionSchema],
+    },
+        
+);
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
