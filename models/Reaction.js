eb60459a58179ff220cd99c3b08b15b2520af4ac
forxtoisate <hwoolford@gmail.com>
2024-02-12 16:16:11 -0600

    added a gitignore file and moved the reaction schema to it's own document

---
+++ models/Reaction.js
@@ -0,0 +1,25 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max_length: 280,
+        },
+        username: { type: String, required: true},
+        createdAt: { type: Date, default: Date.now },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false, // not sure about this part
+    }
+);
+
+module.exports = reactionSchema;
--- models/Thought.js
+++ models/Thought.js
@@ -1,34 +1,24 @@
 const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
 
-const reactionSchema = new Schema(
-    {
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: new ObjectId,
-        },
-        reactionBody: {
-            type: String,
-            required: true,
-            max: 280,
-        },
-        username: { type: String, required: true},
-        createdAt: { type: Date, default: Date.now },
-    }
-);
 
 const thoughtSchema = new Schema(
     {
         thoughtText: {
             type: String,
             required: true,
-            min: 1,
-            max: 280,
+            min_length: 1,
+            max_length: 280,
         },
         createdAt: { type: Date, default: Date.now },
         username: { type: String, required: true },
         reactions: [reactionSchema],
     },
-        
+    {
+        toJSON: {
+            getters: true,
+        },
+    } 
 );
 
 const Thought = model('thought', thoughtSchema);
